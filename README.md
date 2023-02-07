a69b55df47f085a61865327c062568e22312375b
Ricardo Rodriguez <ricardo@reportingstandard.com>
2023-02-07 09:48:57 +0100

    plugin_ready

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # FZF clipboard manager plugin
 ### ZSH plugin to fuzzy search and set into main clipboard items managed by "clipboard indicator" GNOME extension (need to be installed)
+### JQ (json processor) must be installed as well
 
--- fzf-clipboard-indicator.plugin.zsh
+++ fzf-clipboard-indicator.plugin.zsh
@@ -1,20 +1,31 @@
 
 ## Custom binding ALT-x - Select item from clipboard
-fzf-item_clipboard-widget() {
+_fzf-item_clipboard() {
   local items=("${(@f)$(jq '.[].contents' $HOME/.cache/clipboard-indicator@tudmotu.com/registry.txt)}")
   local list=""
   for i in "${items[@]}" 
   do 
     local str="${i:1}"
     str=$(sed 's/.\{1\}$//' <<< "$str")
-    list+="$str"$'\n'
+    list="$str"$'\n'"$list"
   done
   setopt localoptions pipefail no_aliases 2> /dev/null
   local item
-  echo $list | sort -u | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_CTRL_Y_OPTS-}" $(__fzfcmd) -m "$@" | while read item; do
-    echo -n "${(q)item}" | xclip -selection c | echo "Copied"
+  echo $list | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_CTRL_T_OPTS-}" $(__fzfcmd) -m "$@" | while read item; do
+    echo -n "${(q)item}" | xclip -selection c | echo -n "${(q)item}"
   done
+  local ret=$?
+  echo
+  return $ret
 }
+
+fzf-item_clipboard-widget() {
+  LBUFFER="${LBUFFER}$(_fzf-item_clipboard)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+
 zle     -N              fzf-item_clipboard-widget
 bindkey -M emacs '\ex'  fzf-item_clipboard-widget
 bindkey -M vicmd '\ex'  fzf-item_clipboard-widget
